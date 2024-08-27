78c3556259121b658bd9c3bab89e1063d53d18b4
Giedrius Šlikas <slikas.giedrius@gmail.com>
2024-08-27 19:22:31 +0300

    Update HomeScreen.kt

--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/ui/HomeScreen.kt
@@ -1,11 +1,20 @@
 package com.giedrius.slikas.sodybon.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import com.giedrius.slikas.sodybon.data.user.model.User
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.giedrius.slikas.sodybon.getPlatform
 import org.koin.compose.koinInject
 
 @Composable
@@ -13,26 +22,26 @@ fun HomeScreen(
     viewModel: HomeViewModel = koinInject(),
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    Column {
-        Users(uiState.users)
-        Text(uiState.articles.toString())
-    }
-}
 
-@Composable
-fun Users(users: List<User>) {
-    users.forEach {
-        Column {
-            Text(
-                text = it.name
-            )
-            Text(
-                text = it.age.toString()
-            )
-            Text(
-                text = it.fullName
-            )
+    Column {
+        Text(getPlatform().name)
+        LazyColumn {
+            items(uiState.users) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Text(
+                        text = it.fullName,
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
         }
     }
-
 }
