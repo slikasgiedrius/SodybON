f6fbe6ce2ab5eb7241f1a8037833a90d77f3ba00
Giedrius Šlikas <slikas.giedrius@gmail.com>
2024-09-24 17:27:46 +0300

    Add splash screens

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,6 +77,9 @@ kotlin {
 
             //Ktor
             implementation(libs.ktor.client.android)
+
+            //Splash screen
+            implementation(libs.androidx.core.splashscreen)
         }
 
         iosMain.dependencies {
--- composeApp/src/androidMain/kotlin/com/giedrius/slikas/sodybon/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/giedrius/slikas/sodybon/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.giedrius.slikas.sodybon.di.initKoin
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
@@ -13,6 +14,7 @@ import com.google.firebase.ktx.initialize
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(
                 Color.TRANSPARENT, Color.TRANSPARENT
