9c78c3a295000cbe5e570880fff3995a26abc56a
Giedrius Šlikas <slikas.giedrius@gmail.com>
2024-09-24 09:34:06 +0300

    Trigger home screen launch when login is successful

--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/App.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/App.kt
@@ -1,6 +1,8 @@
 package com.giedrius.slikas.sodybon
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.giedrius.slikas.sodybon.compose.base.SodybOnTheme
 import com.giedrius.slikas.sodybon.screens.home.HomeScreen
 import com.giedrius.slikas.sodybon.screens.login.LoginScreen
@@ -9,8 +11,10 @@ import org.koin.compose.koinInject
 
 @Composable
 fun App(loginViewModel: LoginViewModel = koinInject()) {
+    val uiState by loginViewModel.uiState.collectAsState()
+
     SodybOnTheme {
-        if (loginViewModel.uiState.value.currentUser == null) {
+        if (uiState.currentUser == null) {
             LoginScreen()
         } else {
             HomeScreen()
--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/screens/login/LoginViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/screens/login/LoginViewModel.kt
@@ -3,7 +3,6 @@ package com.giedrius.slikas.sodybon.screens.login
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.giedrius.slikas.sodybon.data.user.UserRepository
-import com.giedrius.slikas.sodybon.screens.home.HomeScreenUiState
 import com.mmk.kmpauth.google.GoogleAuthCredentials
 import com.mmk.kmpauth.google.GoogleAuthProvider
 import dev.gitlive.firebase.auth.FirebaseUser
