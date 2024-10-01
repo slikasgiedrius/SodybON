408c09ce9b10a3d74a6354cd8ba183e8dd692d84
Giedrius Šlikas <slikas.giedrius@gmail.com>
2024-10-01 09:48:33 +0300

    Fix PropertyRepositoryImpl a bit

--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/data/property/PropertyRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/data/property/PropertyRepositoryImpl.kt
@@ -1,20 +1,24 @@
 package com.giedrius.slikas.sodybon.data.property
 
+import co.touchlab.kermit.Logger
 import com.giedrius.slikas.sodybon.data.property.model.Property
 import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.firestore.FirebaseFirestoreException
 import dev.gitlive.firebase.firestore.firestore
 
 class PropertyRepositoryImpl(
     private val firestore: Firebase
 ) : PropertyRepository {
+
     override suspend fun getProperties(): List<Property> {
-        try {
+        return try {
             val userResponse = firestore.firestore.collection("PROPERTIES").get()
             return userResponse.documents.map {
                 it.data()
             }
-        } catch (e: Exception) {
-            throw e
+        } catch (e: FirebaseFirestoreException) {
+            Logger.e("Error getting properties ${e.message}")
+            emptyList()
         }
     }
 }
