12dd1b254da679896f1e2edde3cc744e1d2b32fe
Giedrius Šlikas <slikas.giedrius@gmail.com>
2024-10-01 14:28:33 +0300

    Add login screen

--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/App.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/App.kt
@@ -1,12 +1,24 @@
 package com.giedrius.slikas.sodybon
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.giedrius.slikas.sodybon.compose.base.SodybOnTheme
 import com.giedrius.slikas.sodybon.navigation.BottomBar
+import com.giedrius.slikas.sodybon.screens.login.LoginScreen
+import com.giedrius.slikas.sodybon.screens.login.LoginViewModel
+import org.koin.compose.koinInject
 
 @Composable
-fun App() {
+fun App(
+    viewModel: LoginViewModel = koinInject(),
+) {
+    val uiState by viewModel.uiState.collectAsState()
     SodybOnTheme {
-        BottomBar()
+        if (uiState.currentProfile == null) {
+            LoginScreen()
+        } else {
+            BottomBar()
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/screens/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/giedrius/slikas/sodybon/screens/login/LoginScreen.kt
@@ -1,44 +1,133 @@
 package com.giedrius.slikas.sodybon.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.giedrius.slikas.sodybon.compose.base.SodybOnTheme
 import com.giedrius.slikas.sodybon.compose.components.GoogleLogin
+import com.giedrius.slikas.sodybon.data.profile.model.Profile
+import com.giedrius.slikas.sodybon.navigation.BottomBarTabs
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.koin.compose.koinInject
 
 @Composable
 fun LoginScreen(
-    viewModel: LoginViewModel = koinInject(),
+    modifier: Modifier = Modifier,
+    loginViewModel: LoginViewModel = koinInject(),
 ) {
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState by loginViewModel.uiState.collectAsState()
 
-    MaterialTheme {
-        Column(
-            Modifier.fillMaxSize().padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)
+    SodybOnTheme {
+        LoginScreenContent(
+            currentProfile = uiState.currentProfile,
+            modifier = modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+                .windowInsetsPadding(WindowInsets.safeDrawing),
+            onUpdateUserClicked = { loginViewModel.updateUser() },
+        )
+    }
+}
+
+
+@Composable
+fun LoginScreenContent(
+    currentProfile: Profile?,
+    modifier: Modifier = Modifier,
+    onUpdateUserClicked: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        // top row
+        Row(
+            modifier = Modifier.fillMaxWidth().height(50.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            GoogleLogin(
-                onUpdateUser = { viewModel.updateUser() },
-            )
-
             Column {
-                Text(
-                    text = "User ${uiState.currentProfile?.fullName}",
-                    style = MaterialTheme.typography.body1,
-                    textAlign = TextAlign.Start,
-                )
+                if (currentProfile == null) {
+                    GoogleLogin(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        onUpdateUser = { onUpdateUserClicked() },
+                    )
+                } else {
+                    Row {
+                        if (currentProfile.photoUrl != null) {
+                            KamelImage(modifier = Modifier.size(48.dp).clip(CircleShape),
+                                resource = asyncPainterResource(currentProfile.photoUrl),
+                                contentDescription = "",
+                                contentScale = ContentScale.Fit,
+                                onLoading = { CircularProgressIndicator(it) },
+                                onFailure = {
+                                    Column {
+                                        Text(
+                                            text = "Failed to load",
+                                            fontWeight = FontWeight.Bold,
+                                        )
+                                    }
+                                })
+                        } else {
+                            Icon(
+                                Icons.Default.AccountCircle,
+                                contentDescription = currentProfile.firstName
+                                    ?: BottomBarTabs.Profile.name,
+                            )
+                        }
+                        Text(
+                            text = currentProfile.fullName ?: "Not logged in",
+                            style = MaterialTheme.typography.h6,
+                            fontWeight = FontWeight.SemiBold,
+                        )
+                    }
+                }
             }
+
+        }
+
+        // content row
+        Row(
+            modifier = Modifier.fillMaxWidth().weight(0.5F),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Some Other Contents")
+        }
+
+        // bottom row
+        Row(
+            modifier = Modifier.fillMaxWidth().height(100.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Bottom Content")
         }
     }
 }
